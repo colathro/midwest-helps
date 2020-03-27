fd215d555a40865adb900cf24f6782f6ebab1594
Colton Lathrop <colathro@microsoft.com>
2020-03-27 18:28:27 -0500

    rip and scoot

---
+++ Controllers/ListingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace getthehotdish.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ListingController : ControllerBase
+    {
+
+
+        private readonly ILogger<ListingController> _logger;
+
+        public ListingController(ILogger<ListingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
