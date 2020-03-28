1560460bb5e76611465a773e71fa5c15545adc04
Daniel Dantas <danieldnds@gmail.com>
2020-03-27 21:48:01 -0700

    renaming custom.scss to App.scss

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
 import { Home } from './components/Home';
-import './custom.scss';
+import './App.scss';
 
 import { Layout } from 'antd';
 import { Typography } from 'antd';
@@ -12,7 +12,7 @@ const { Title } = Typography;
 export const App: React.FC = () => (
   <Layout>
     <Header className="header">
-      <Title style={{color:"white"}}>Hotdish</Title>
+      <Title>Hotdish</Title>
     </Header>
     <Content style={{ padding: '50px 50px' }}>
       <Layout className="site-layout-background" style={{ padding: '24px 0'}}>
