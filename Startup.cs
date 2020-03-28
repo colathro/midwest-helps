1a46f49beb5f137d4203a90b96e180a0a7c4d531
Colton Lathrop <colathro@microsoft.com>
2020-03-27 20:19:58 -0500

    db + apikey secret managed

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,14 @@
+using getthehotdish.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 
 namespace getthehotdish
 {
@@ -20,8 +24,21 @@ namespace getthehotdish
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
+
+            services.AddDbContext<DataContext>(options => 
+                options.UseCosmos("https://getthehotdish.documents.azure.com:443/",
+                Configuration["COSMOS_API_KEY"],
+                databaseName: "getthehotdish")
+            );
+
+
+            services.AddLogging(builder =>
+            {
+                builder.AddApplicationInsights("d05672ef-8c2d-406e-bd03-bdc0888ed25d");
+                builder.AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Information);
+                builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Error);
+            });
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
