461cdec41d86a9bd0e50387bd8843e4897000896
Colton <42881380+colathro@users.noreply.github.com>
2020-03-28 10:25:50 -0500

    refactor + styling (#5)

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router';
-import { Home } from './components/Home';
+import { Home } from './components/Home/Home';
 import './App.scss';
 
 import { Layout } from 'antd';
@@ -11,26 +11,6 @@ const { Title, Text } = Typography;
 
 export const App: React.FC = () => (
   <Layout>
-    <Header className="header-layout">
-      <Title className="header-title" level={3}>Hotdish</Title>
-    </Header>
-    <Content className="content-layout">
-      <Layout className="layout-purple-background">
-        <Title className="title">Dishin' out hot updates for business in the United States</Title>
-        <Title className="title" level={4}>
-          The temporary shut down of US businesses due to COVID-19 has many folks struggling.
-          This site is meant to be a resource for people to dish up on the latest info and continue to support their favorite local spots.
-          This information is crowdsourced, so please verify the accuracy independently. If you see a mistake or need to update a post, please contact us.
-        </Title>
-      </Layout>
-    </Content>
-    <Content className="content-layout-main">
-      <Layout className="layout-white-background">
-        <Content className="content-home">
           <Route exact path='/' component={Home} />
-        </Content>
-      </Layout>
-    </Content>
-    <Footer className="footer-layout">🧡 Made by Jordan, Colton, Daniel, and Louie 💌 Hello@getthehotdish.com</Footer>
   </Layout>
 );
---
+++ DataAccess/BusinessType.cs
@@ -0,0 +1,15 @@
+namespace getthehotdish.DataAccess
+{
+    public enum BusinessType
+    {
+        Brewery = 0,
+        Coffee = 1,
+        Entertainment = 2,
+        Grocery = 3,
+        Other = 4,
+        Religious = 5,
+        Restaurant = 6,
+        Retail = 7,
+        Wellness = 8
+    }
+}
