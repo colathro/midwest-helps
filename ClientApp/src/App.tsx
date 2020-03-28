8d2485b0e3d009b7a1bbccbd672bd3c5d5c202d2
Daniel Dantas <danieldnds@gmail.com>
2020-03-27 21:07:39 -0700

    Layout created

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,12 +1,27 @@
 import React, { FunctionComponent } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-
 import './custom.scss';
+
+import { Layout } from 'antd';
+import { Typography } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { Title } = Typography;
 
 export const App: FunctionComponent = () => (
   <Layout>
-    <Route exact path='/' component={Home} />
+    <Header className="header">
+      <Title style={{color:"white"}}>Hotdish</Title>
+    </Header>
+    <Content style={{ padding: '50px 50px' }}>
+      <Layout className="site-layout-background" style={{ padding: '24px 0'}}>
+        <Sider className="site-layout-background" width={200}>
+          Sider
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
+      </Layout>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
   </Layout>
 );
