523e406a43f9f3e3ecbe4f393c3d81564924785a
Louie Bertoncin 🥏 <louiebertoncin@gmail.com>
2020-03-30 15:40:36 -0700

    Unregistering the service worker

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -14,4 +14,4 @@ ReactDOM.render(
   rootElement
 );
 
-registerServiceWorker();
+unregister();
