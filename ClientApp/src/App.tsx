27b57e27a1dc6a987ad05e33349f6811092a8ade
Daniel Dantas <danieldnds@gmail.com>
2020-03-29 17:32:50 -0700

    document title added

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,17 +1,19 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react';
-import { Route } from 'react-router';
-import { Layout, Row, Col } from 'antd';
-import { Home } from './components/Home';
-import { Contact } from './components/Contact';
-import { List } from './components/List';
+import React from "react";
+import { Route } from "react-router";
+import { Layout, Row, Col } from "antd";
+import { Home } from "./components/Home";
+import { Contact } from "./components/Contact";
+import { List } from "./components/List";
+import DocumentTitle from "react-document-title";
 
-import './App.scss';
+import "./App.scss";
 
 const { Footer, Content } = Layout;
 
 export const App: React.FC = () => (
   <Layout>
+    <DocumentTitle title="Hotdish—Support small businesses" />
     <Content className="main-content">
       <Route exact path="/" component={Home} />
       <Route path="/contact" component={Contact} />
