016266a11cb52b658f8d01c528bb167d4df17456
Daniel Dantas <danieldnds@gmail.com>
2020-03-31 19:43:07 -0700

    BusinessChannelType and DeliveryAppType enums created

---
+++ DataAccess/BusinessChannelType.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace getthehotdish.DataAccess
+{
+    [Flags]
+    public enum BusinessChannelType
+    {
+        None = 0,
+        CurbSide = 1,
+        TakeOut = 2,
+        DriveThru = 4,
+        Delivery = 8,
+        LiveStream = 16,
+        AppointmentOnly = 32
+    }
+}
