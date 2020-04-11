b07a469b4d8e97730a8953fbbdb1e515bf6841ba
Colton Lathrop <colathro@microsoft.com>
2020-04-10 20:08:27 -0500

    gogogog

---
+++ Models/AdminSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace getthehotdish.Models
+{
+    public class AdminSettings
+    {
+        public string Key { get; set; }
+    }
+}
