e31cf2427e186f73320ff99fede7ffbbbaf860a3
Colton <42881380+colathro@users.noreply.github.com>
2020-04-21 13:05:34 -0500

    i bend the knee to dto even though it sucks, annoying to work with,  and i hate it alot - thanks for listening (#71)

---
+++ Controllers/MaskRequestController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using getthehotdish.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using getthehotdish.DataAccess;
+
+namespace getthehotdish.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaskRequestController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public MaskRequestController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MaskRequestModel>>> GetMaskRequests()
+        {
+            return await _context.MaskRequests.Select(m => MaskRequestModel.ConvertToDTO(m)).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MaskRequestModel>> GetMaskRequest(Guid id)
+        {
+            var maskRequest = await _context.MaskRequests.FindAsync(id);
+
+            if (maskRequest == null)
+            {
+                return NotFound();
+            }
+
+            return MaskRequestModel.ConvertToDTO(maskRequest);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MaskRequest>> PostMaskRequest(MaskRequestModel maskRequest)
+        {
+            maskRequest.PartitionKey = "MR";
+            maskRequest.CreatedOn = DateTime.UtcNow;
+
+            MaskRequest dbo = maskRequest.ConvertToDBO();
+
+            _context.MaskRequests.Add(dbo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMaskRequest", new { id = maskRequest.Id }, maskRequest);
+        }
+
+        private bool MaskRequestExists(Guid id)
+        {
+            return _context.MaskRequests.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/MaskRequestModel.cs
@@ -0,0 +1,39 @@
+﻿using getthehotdish.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace getthehotdish.Models
+{
+    public class MaskRequestModel
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("partitionKey")]
+        public string PartitionKey { get; set; }
+
+        [JsonPropertyName("createdOn")]
+        public DateTime CreatedOn { get; set; }
+
+        public MaskRequest ConvertToDBO()
+        {
+            return new MaskRequest()
+            {
+                PartitionKey = this.PartitionKey,
+                CreatedOn = this.CreatedOn
+            };
+        }
+
+        public static MaskRequestModel ConvertToDTO(MaskRequest mr)
+        {
+            return new MaskRequestModel
+            {
+                Id = mr.Id,
+                PartitionKey = mr.PartitionKey,
+                CreatedOn = mr.CreatedOn
+            };
+        }
+    }
+}
