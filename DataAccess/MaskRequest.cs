5dfe7fb0336901107d68ee2383a89932c7e86c87
Colton Lathrop <colathro@microsoft.com>
2020-04-22 20:15:26 -0500

    api approvals for masks

--- DataAccess/MaskRequest.cs
+++ DataAccess/MaskRequest.cs
@@ -10,5 +10,8 @@ namespace getthehotdish.DataAccess
         public Guid Id { get; set; }
         public string PartitionKey { get; set; }
         public DateTime CreatedOn { get; set; }
+        public Guid EditKey { get; set; }
+        public Guid OriginalId { get; set; }
+        public bool Approved { get; set; }
     }
 }
