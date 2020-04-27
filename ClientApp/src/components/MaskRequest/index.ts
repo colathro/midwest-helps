65f216979941de4aae2c3b3dd10b5a184a858faf
Daniel Dantas <danieldnds@gmail.com>
2020-04-26 20:08:26 -0700

    Mask request page created

---
+++ ClientApp/src/components/MaskRequest/index.ts
@@ -0,0 +1,1 @@
+export { MaskRequest } from './MaskRequest';
--- ClientApp/src/components/Masks/Masks.tsx
+++ ClientApp/src/components/Masks/Masks.tsx
@@ -1,39 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './Masks.scss';
-import { Button, Modal } from 'antd';
-import { MaskRequestForm } from '../MaskRequestForm';
+import { Button } from 'antd';
 
 export const Masks: React.FC = () => {
-  const [displayMaskRequest, setDisplayMaskRequest] = useState(false);
-
-  const displayMaskRequestForm = (display: boolean) => {
-    setDisplayMaskRequest(display);
-  };
-
-  const handleCancel = () => {
-    displayMaskRequestForm(false);
-  };
-
-  const maskRequestModal = (
-    <Modal
-      visible={displayMaskRequest}
-      title="MidwestHelps"
-      onCancel={handleCancel}
-      footer={null}
-    >
-      <MaskRequestForm />
-    </Modal>
-  );
+  const history = useHistory();
 
   return (
     <>
-      {displayMaskRequest && maskRequestModal}
-      <Button
-        type="primary"
-        onClick={() => {
-          displayMaskRequestForm(true);
-        }}
-      >
+      <Button type="primary" onClick={() => history.push('/maskRequest')}>
         Request masks
       </Button>
     </>
