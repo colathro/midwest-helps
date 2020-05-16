e0e143851fb87b3101e2f1961cb382e1b2a4dabe
Daniel Dantas <danieldnds@gmail.com>
2020-05-16 01:23:59 -0700

    Users/danieldnds/email framework (#107)
    * EmailUtils + Email templates created
    
    * sending email methods created + DonationStatus created
    
    * EmailUtils update
    
    * donation update status created
    
    * Donation status page created

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -4,11 +4,12 @@ import { Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
 import { Admin } from './components/Admin';
 import { Contact } from './components/Contact';
+import { DonationStatus } from './components/DonationStatus';
 import { List } from './components/List';
 import { Masks } from './components/Masks';
 import { MaskRequest } from './components/MaskRequest';
 import { Covid19 } from './components/Covid19';
-import { PrivateRoute } from './components/AdminLogin/PrivateRoute'
+import { PrivateRoute } from './components/AdminLogin/PrivateRoute';
 import DocumentTitle from 'react-document-title';
 
 import './App.scss';
@@ -25,6 +26,7 @@ export const App: React.FC = () => (
         <Route exact path="/maskRequest" component={MaskRequest} />
         <Route exact path="/list" component={List} />
         <Route exact path="/covid19" component={Covid19} />
+        <Route exact path="/donationStatus" component={DonationStatus} />
         <Route path="/" component={Masks} />
       </Switch>
     </Content>
