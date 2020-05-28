a87d9e43d11dceb344b220687a44c387e4f05cc3
Colton Lathrop <colt@Coltons-MacBook-Pro.local>
2020-05-27 22:42:50 -0500

    sync email send

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -47,7 +47,7 @@ namespace getthehotdish.Controllers
                 _dataContext.Contacts.Add(contact);
                 await _dataContext.SaveChangesAsync();
 
-                _ = EmailUtils.SendEmailAsync(_emailSettings, EmailMessageType.Contact, "Thanks for contacting us!", "Thanks for contacting us!", contact.Email, contact.Name);
+                await EmailUtils.SendEmailAsync(_emailSettings, EmailMessageType.Contact, "Thanks for contacting us!", "Thanks for contacting us!", contact.Email, contact.Name);
 
                 return Ok();
             }
