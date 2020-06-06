e3461f0082cbf1e723e31e0a1e4eb4ef39d80ece
Colton Lathrop <colathro@microsoft.com>
2020-06-06 15:12:02 -0500

    changes

--- ClientApp/src/components/Cookie/Cookie.tsx
+++ ClientApp/src/components/Cookie/Cookie.tsx
@@ -10,207 +10,64 @@ const { Title } = Typography;
 export const Cookie: React.FC = () => {
   return (
     <NavBar>
-      Privacy Policy Last updated [May 29th, 2020] Our Privacy Policy forms part
-      of and must be read in conjunction with, website Terms and Conditions. We
-      reserve the right to change this Privacy Policy at any time. We respect
-      the privacy of our users and every person who visits our sites
-      www.midwesthelps.com. Here at, Midwest Helps (“we”, “us”, or “our”), we
-      are committed to protecting your personal information and your right to
-      privacy in accordance with General Data Protection Regulation (GDPR). If
-      you have any questions or concerns about our policy or our practices with
-      regards to your personal information, please contact us at
-      info@midwesthelps.com. When you visit our website www.midwesthelps.com
-      (“Site”, and use our services, you trust us with your personal
-      information. We take your privacy very seriously. In this privacy notice,
-      we describe our privacy policy. We seek to explain to you in the clearest
-      way possible what information we collect, how we use it and what rights
-      you have in relation to it. We hope you take some time to read through it
-      carefully, as it is important. If there are any terms in this privacy
-      policy that you do not agree with, please discontinue the use of our site
-      and our services. This privacy policy applies to all information collected
-      through our website, and/or any related services, sales, marketing or
-      events (we refer to them collectively in this privacy policy as the
-      “Site“). ABOUT US Midwest Helps is focused on providing a bulletin board
-      for businesses and organizations to get connected to awesome makers. Our
-      office is in United States. Please read this privacy policy carefully as
-      it will help you make informed decisions about sharing your personal
-      information with us. 1. WHAT INFORMATION DO WE COLLECT? The Personal
-      Information you disclose to us We collect personal information that you
-      voluntarily provide to us when expressing an interest in obtaining
-      information about us or our products and services when participating in
-      activities on the Site or otherwise contacting us. The personal
-      information that we collect depends on the context of your interactions
-      with us and the Site, the choices you make and the products and features
-      you use. The personal information we collect can include the following:
-      Name and Contact Data. We collect your first and last name, email address,
-      postal address, phone number, and other similar contact data. Credentials.
-      We collect passwords, password hints, and similar security information
-      used for authentication and account access. Information automatically
-      collected We automatically collect certain information when you visit, use
-      or navigate the Site. This information does not reveal your specific
-      identity (like your name or contact information) but may include device
-      and usage information, such as your IP address, browser and device
-      characteristics, operating system, language preferences, referring URLs,
-      device name, country, location, information about how and when you use our
-      Site and other technical information. If you access our site with your
-      mobile device, we may automatically collect device information (such as
-      your mobile device ID, model and manufacturer), operating system, version
-      information and IP address. This information is primarily needed to
-      maintain the security and operation of our Site, and for our internal
-      analytics and reporting purposes. Like many businesses, we also collect
-      information through cookies and similar technologies. You can find out
-      more about this in our Cookie Policy. Information collected from other
-      Sources We may obtain information about you from other sources, such as
-      public databases, joint marketing partners, social media platforms (such
-      as Facebook), as well as from other third parties. 2. HOW DO WE USE YOUR
-      INFORMATION? We use your personal information for these purposes in
-      reliance on our legitimate business interests (“Business Purposes”), in
-      order to enter into or perform a contract with you (“Contractual”), with
-      your consent (“Consent”), and/or for compliance with our legal obligations
-      (“Legal Reasons”). We indicate the specific processing grounds we rely on
-      next to each purpose listed below. We use the information we collect or
-      receive: • To send you marketing and promotional communications for
-      Business Purposes and/or with your Consent. We and/or our third party
-      marketing partners may use the personal information you send to us for our
-      marketing purposes, if this is in accordance with your marketing
-      preferences. You can opt-out of our marketing emails at any time (see
-      below). • To send administrative information to you for related to your
-      account, our business purposes, and/or for legal reasons. We may use your
-      personal information to send you product, service and new feature
-      information and/or information about changes to our terms, conditions, and
-      policies. • Deliver targeted advertising to you for our Business Purposes
-      and/or with your Consent. We may use your information to develop and
-      display content and advertising (and work with third parties who do so)
-      tailored to your interests and/or location and to measure its
-      effectiveness. [For more information, see our Cookie Policy. • Request
-      Feedback for our Business Purposes and/or with your Consent. We may use
-      your information to request feedback and to contact you about your use of
-      our Site. • To protect our Site for Business Purposes and/or Legal
-      Reasons. We may use your information as part of our efforts to keep our
-      Site safe and secure (for example, for fraud monitoring and prevention). •
-      To enable user-to-user communications with your consent. We may use your
-      information in order to enable user-to-user communications with each
-      user’s consent. • To enforce our terms, conditions and policies for our
-      business purposes and as legally required. • To respond to legal requests
-      and prevent harm as legally required. If we receive a subpoena or other
-      legal request, we may need to inspect the data we hold to determine how to
-      respond. • For other Business Purposes. We may use your information for
-      other Business Purposes, such as data analysis, identifying usage trends,
-      determining the effectiveness of our promotional campaigns and to evaluate
-      and improve our Site, products, services, marketing and your experience.
-      3. WILL YOUR INFORMATION BE SHARED WITH ANYONE? We only share and disclose
-      your information in the following situations: • Compliance with Laws. We
-      may disclose your information where we are legally required to do so in
-      order to comply with applicable law, governmental requests, a judicial
-      proceeding, court order, or legal process, such as in response to a court
-      order or a subpoena (including in response to public authorities to meet
-      national security or law enforcement requirements). • Vital Interests and
-      Legal Rights. We may disclose your information where we believe it is
-      necessary to investigate, prevent, or take action regarding potential
-      violations of our policies, suspected fraud, situations involving
-      potential threats to the safety of any person and illegal activities, or
-      as evidence in litigation in which we are involved. • Vendors, Consultants
-      and Other Third-Party Service Providers. We may share your data with third
-      party vendors, service providers, contractors or agents who perform
-      services for us or on our behalf and require access to such information to
-      do that work. • Business Transfers. We may share or transfer your
-      information in connection with, or during negotiations of, any merger,
-      sale of company assets, financing, or acquisition of all or a portion of
-      our business to another company. • Third-Party Advertisers. We may use
-      third-party advertising companies to serve ads when you visit the Site.
-      These companies may use information about your visits to our Site and
-      other websites that are contained in web cookies and other tracking
-      technologies in order to provide advertisements about goods and services
-      of interest to you. See our Cookie Policy for further information. •
-      Affiliates. We may share your information with our affiliates, in which
-      case we will require those affiliates to honor this privacy policy.
-      Affiliates include our parent company and any subsidiaries, joint venture
-      partners or other companies that we control or that are under common
-      control with us. • Business Partners. We may share your information with
-      our business partners to offer you certain products, services or
-      promotions. • With your Consent. We may disclose your personal information
-      for any other purpose with your consent. • Other Users. When you share
-      personal information (for example, by posting comments, contributions or
-      other content to the Site) or otherwise interact with public areas of the
-      Site, such personal information may be viewed by all users and may be
-      publicly distributed outside the Site in perpetuity. 4. DO WE USE COOKIES
-      AND OTHER TRACKING TECHNOLOGIES? We may use cookies and similar tracking
-      technologies (like web beacons and pixels) to access or store information.
-      Specific information about how we use such technologies and how you can
-      refuse certain cookies is set out in our Cookie Policy. 5. IS YOUR
-      INFORMATION TRANSFERRED INTERNATIONALLY? Our website server is located at
-      ______________. We will not transfer your personal information to an
-      overseas recipient. 6. WHAT IS OUR STANCE ON THIRD-PARTY WEBSITES? The
-      Site may contain advertisements from third parties that are not affiliated
-      with us and which may link to other websites, online services or mobile
-      applications. We cannot guarantee the safety and privacy of data you
-      provide to any third parties. Any data collected by third parties is not
-      covered by this privacy policy. We are not responsible for the content or
-      privacy and security practices and policies of any third parties,
-      including other websites, services or applications that may be linked to
-      or from the Site. You should review the policies of such third parties and
-      contact them directly to respond to your questions. 7. HOW LONG DO WE KEEP
-      YOUR INFORMATION? We will only keep your personal information for as long
-      as it is necessary for the purposes set out in this privacy policy, unless
-      a longer retention period is required or permitted by law (such as tax,
-      accounting or other legal requirements). No purpose in this policy will
-      require us keeping your personal information for longer than 90 days past
-      the termination of your account. When we have no ongoing legitimate
-      business need to process your personal information, we will either delete
-      or anonymize it, or, if this is not possible (for example, because your
-      personal information has been stored in backup archives), then we will
-      securely store your personal information and isolate it from any further
-      processing until deletion is possible. 8. HOW DO WE KEEP YOUR INFORMATION
-      SAFE? We have implemented appropriate technical and organizational
-      security measures designed to protect the security of any personal
-      information we process. However, please also remember that we cannot
-      guarantee that the internet itself is 100% secure. Although we will do our
-      best to protect your personal information, transmission of personal
-      information to and from our Site is at your own risk. You should only
-      access the services within a secure environment. 9. DO WE COLLECT
-      INFORMATION FROM MINORS? We do not knowingly solicit data from or market
-      to children under 16 years of age. By using the Site, you represent that
-      you are at least 16 or that you are the parent or guardian of such a minor
-      and consent to such minor dependent’s use of the Site. If we learn that
-      personal information from users less than 16 years of age has been
-      collected, we will deactivate the account and take reasonable measures to
-      promptly delete such data from our records. If you become aware of any
-      data we have collected from children under age 16, please contact us at
-      info@midwesthelps.com. 10. GDPR ENTITLEMENT EU General Data Protection
-      Regulation (GDPR) has provided the below rights to the EU residents: Right
-      to information - the purposes for processing Personal Information and the
-      rights of the individual. Right to access the Personal Information that
-      are processed. Right to erasure (”Right to be forgotten”). Right to
-      rectification. Right to restriction of processing. Right to object
-      (opt-out) to processing. EU residents can exercise these rights by raising
-      a request directly at info@midwesthelps.com. 11. WHAT ARE YOUR PRIVACY
-      RIGHTS? .Personal Information You may at any time review or change the
-      information in your account or terminate your account by: • Logging into
-      your account settings and updating your account • Contacting us using the
-      contact information provided below Upon your request to terminate your
-      account, we will deactivate or delete your account and information from
-      our active databases. However, some information may be retained in our
-      files to prevent fraud, troubleshoot problems, assist with any
-      investigations, enforce our Terms of Use and/or comply with legal
-      requirements. Cookies and similar technologies: Most Web browsers are set
-      to accept cookies by default. If you prefer, you can usually choose to set
-      your browser to remove cookies and to reject cookies. If you choose to
-      remove cookies or reject cookies, this could affect certain features or
-      services of our Site. 12. DO WE MAKE UPDATES TO THIS POLICY? We may update
-      this privacy policy from time to time. The updated version will be
-      indicated by an updated “Revised” date and the updated version will be
-      effective as soon as it is accessible. If we make material changes to this
-      privacy policy, we may notify you either by prominently posting a notice
-      of such changes or by directly sending you a notification. We encourage
-      you to review this privacy policy frequently to be informed of how we are
-      protecting your information. 13. DATA PROTECTION OFFICER We have appointed
-      a Data Protection Officer (“DPO”) who is responsible for overseeing
-      questions in relation to this privacy notice. If you have any questions
-      about this privacy notice, including any requests to exercise your legal
-      rights, please contact Data Protection Officer, at our e-mail
-      info@midwesthelps.com. 14. HOW CAN YOU CONTACT US ABOUT THIS POLICY? If
-      you have questions or comments about this policy, email us at
-      info@midwesthelps.com
+      <Title>Cookie Policy</Title>
+      <p>
+        <b>
+          BY CONTINUING TO USE OUR SITE AND SERVICES, YOU ARE AGREEING TO THE
+          USE OF COOKIES AND SIMILAR TECHNOLOGIES FOR THE PURPOSES WE DESCRIBE
+          IN THIS COOKIES POLICY. IF YOU DO NOT ACCEPT THE USE OF COOKIES AND
+          SIMILAR TECHNOLOGIES, DO NOT USE THIS SITE.
+        </b>
+      </p>
+      <b>Cookies:</b>
+      <p>
+        A cookie is a small text file that a website saves on your computer or
+        mobile device when you visit the site. In general, cookies have two main
+        purposes: to improve your browsing experience by remembering your
+        actions and preferences, and to help us analyze our website traffic.
+      </p>
+      <b>What to do with Cookies?</b>
+      <p>
+        We use cookies to help us analyze traffic to the Website, to help us
+        improve website performance and usability, and to make the Website more
+        secure. Third-party cookies help us use Google Analytics to count, track
+        and analyze visits to the Website. This helps us understand how people
+        are using our websites and where we need to make improvements. These
+        third-party cookies do not specifically identify you.
+      </p>
+      <b>Types & Category of Cookies used:</b>
+      <p>
+        List category of cookies used in https://midwesthelps.com. For example:
+        <ul>
+          <li>
+            <b>Security</b>
+            <p>
+              We use cookies to enable and support our security features, and to
+              help us detect malicious activity violations of our Terms and
+              Conditions.
+            </p>
+          </li>
+          <li>
+            <b>Performance, Analytics, Research & Advertising</b>
+          </li>
+        </ul>
+      </p>
+      <p>
+        Cookies help us learn how well our site and web products perform in
+        different locations. We also use these to understand, improve, and
+        research products, features, and services, including when you access
+        this site https://midwesthelps.com/ from other websites, applications,
+        or devices such as your work computer or your mobile device. We also use
+        third party cookies to improve and personalize our marketing
+        messages/communications with you.
+      </p>
+      <b>Control cookies</b>
+      <p>
+        You are always free to delete cookies that are already on your computer
+        through your browser settings, and you can set most browsers to prevent
+        them from being added to your computer. However, this may prevent you
+        from using certain features on the Website.
+      </p>
       <FolksLinker />
     </NavBar>
   );
